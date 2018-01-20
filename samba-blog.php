aad3c6f1d451c97c1cfe6585ca0ce5ea4af794d9
Mark Centoni <markcentoni@Marks-MacBook-Pro.local>
2018-01-19 23:03:36 -0800

    first commit

---
+++ samba-blog.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Samba Blog Plugin
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Samba TV blog templates
+Version:      20180119
+Author:       Samba TV
+Author URI:   https://platform.samba.tv
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  sambablog
+Domain Path:  /languages
+*/
